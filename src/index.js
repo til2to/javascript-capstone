3b42e58d76091a0f639e3ac3b88ec79de4b527fb
Supreeti <sksupreetk11@gmail.com>
2023-05-24 13:21:07 +0530

    add popup for homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+document.querySelector('#show-comment').addEventListener('click', function(){
+document.querySelector('.popup').classList.add('active');
+});
+
+document.querySelector('.popup .close-btn').addEventListener('click', function(){
+document.querySelector('.popup').classList.add('active');
+});
