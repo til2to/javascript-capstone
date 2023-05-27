3c1359df2b0397ac512d3eb61648d936b284e2f0
til2to <padwusup@gmail.com>
2023-05-27 01:27:07 +0000

    Add item counter for rockets

--- item-counter.test.js
+++ item-counter.test.js
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
 */
+import { getRocketCount } from './src/index.js';
 
 // Mock localStorage
 const localStorageMock = (() => {
@@ -20,35 +21,35 @@ Object.defineProperty(window, 'localStorage', {
   value: localStorageMock,
 });
 
-describe('completeTask function', () => {
+describe('returns the correct rocket count', () => {
   beforeEach(() => {
     localStorageMock.clear();
-    const task = [{ index: 1, description: 'Task 1', completed: false }];
-    localStorageMock.setItem('tasks', JSON.stringify(task));
+    const spaceX = [
+      { rocket_id: 'falcon1' },
+    ];
+    localStorageMock.setItem('rocketData', JSON.stringify(spaceX));
   });
 
-  test('completeTask updates the completion status of a task in local storage', () => {
-    // Arrange or Set up initial tasks array
-    // const tasks = [
-    //   { index: 1, description: 'Task 1', completed: false },
-    //   { index: 2, description: 'Task 2', completed: false },
-    //   { index: 3, description: 'Task 3', completed: false },
-    // ];
+  test('get the length of the rockets in the local storage', () => {
+    // Arrange or Set up initial rockets array
+    const rockets = [
+      { index: 1, description: 'Rocket 1' },
+      { index: 2, description: 'Rocket 2' },
+      { index: 3, description: 'Rocket 3' },
+    ];
 
-    // // Set tasks array in local storage
-    // localStorageMock.setItem('tasks', JSON.stringify(tasks));
+    // Retrieve local storage
+    const updatedRockets = JSON.parse(localStorageMock.getItem('rocketData')) || [];
+    
+    // Set rockets array in local storage
+    localStorageMock.setItem('rocketData', JSON.stringify([...updatedRockets, ...rockets]));
 
-    // // Act or Call completeTask to update the completion status of Task 1 to true
-    // completeTask(1, true);
+    const expectedLength = 4;
 
-    // // Retrieve updated tasks array from local storage
-    // const updatedTasks = JSON.parse(localStorageMock.getItem('tasks'));
+    // Act or Call getRocketCount function
+    const rocketCount = getRocketCount();
 
-    // // Asset or Verify that Task 1 completion status is updated
-    // expect(updatedTasks[0].completed).toBe(true);
-
-    // // Verify that Task 2 and Task 3 completion statuses are unchanged
-    // expect(updatedTasks[1].completed).toBe(false);
-    // expect(updatedTasks[2].completed).toBe(false);
+    // Assert or Verify the rocket count
+    expect(rocketCount).toEqual(expectedLength);
   });
 });
